f44a397137f06f4e355d727f0439e2ff1d58389f
giulioPasin <g.pasinstudent.uw.edu.pl>
2024-12-30 15:04:35 +0100

    final problem 4

--- optimization/applied_cons_sav_update.jl
+++ optimization/applied_cons_sav_update.jl
@@ -1,5 +1,3 @@
-using Pkg
-Pkg.add("LaTeXStrings")
 using Plots, NLopt, LaTeXStrings
 
 ## A function to create a generic household object:
@@ -41,9 +39,9 @@ hh_01   = create_HH(σ=0.1)
 hh_1    = create_HH(σ=1)
 hh_2    = create_HH(σ=2)
 
-hh_01.u(1)
-hh_1.u(1)
-hh_2.u(1)
+hh_01.u(1.0)
+hh_1.u(1.0)
+hh_2.u(1.0)
 
 ## Plot the utility as a function of consumption for different σ  
 plot(hh_01.u,0.3,10,xlabel="Consumption", label="σ=0.1",ylabel="u(c)",lw=3)
@@ -136,73 +134,9 @@ end
 ## Define a household modelled:
 hh    = create_HH(y=[1,4,3,0.5,0.5],a_0=1)
 
-# Task 1: nlopt_objective_fn function
-
-# Task 1: Filling in the missing parts in the nlopt_objective_fn function
-
-function nlopt_objective_fn(a, grad, hh)
-    # Define the rate of return, initial assets, and income for each period from the hh object
-    R = hh.R
-    a_0 = hh.a_0
-    y = hh.y 
-    # Calculate consumption for each period using the formula provided
-    c_1 = R * a_0 * y[1] - a[1]
-    c_2 = R * a_0 * y[2] - a[2]
-    c_3 = R * a_0 * y[3] - a[3]
-    c_4 = R * a_0 * y[4] - a[4]
-    c_5 = R * a_0 * y[5] 
-
-    # Calculate lifetime utility (assuming some utility function, e.g., logarithmic utility)
-    Lifetime_util = 0
-    utility_discount_rate = hh.β
-    
-    # Assuming a simple logarithmic utility function for demonstration:
-    for t in 1:5
-        c_t = eval(Symbol("c_$t"))  # Get the consumption value (c_1, c_2, ..., c_5)
-        Lifetime_util += (utility_discount_rate^(t-1)) * log(c_t)  # Log utility (example)
-    end
-
-    # Print the parameters and utility value for debugging
-    println("Params: ", round.(a, digits=5), ", Lifetime Utility: ", round(Lifetime_util, digits=5))
-    
-    return -Lifetime_util  # Return negative utility since NLopt minimizes by default
-end
-
-function nlopt_objective_fn(a, grad, hh)
-    # Define parameters from the household object
-    R = hh.R
-    a_0 = hh.a_0
-    y = hh.y
-    β = hh.β  # Discount factor
-    
-    # Calculate consumption for each period
-    T = length(y)  # Number of periods
-    c = Vector{Float64}(undef, T)  # Initialize consumption array
-    c[1] = R * a_0 + y[1] - a[1]  # First period includes initial assets
-    for t in 2:T
-        c[t] = R * a[t-1] + y[t] - a[t]  # Subsequent periods
-    end
-
-    # Calculate lifetime utility
-    Lifetime_util = 0.0
-    for t in 1:T
-        if c[t] <= 0
-            println("Warning: Non-positive consumption in period $t. Returning -Inf.")
-            return Inf  # Penalize infeasible solutions
-        end
-        Lifetime_util += (β^(t-1)) * log(c[t])  # Log utility function
-    end
-
-    # Debugging output
-    println("Assets: ", round.(a, digits=5), ", Lifetime Utility: ", round(Lifetime_util, digits=5))
-    
-    return -Lifetime_util  # NLopt minimizes the objective function by default
-end
-
-
-####### TASK 2: Define the correct dimensionality: #######
-opt = NLopt.Opt(:LN_COBYLA,4) 
-
+## Define the optimizer used:
+####### TASK 2: Define a correct dimensionality: #######
+opt = NLopt.Opt(:LN_COBYLA, 0) 
 ## Define the objective function:
 NLopt.max_objective!(opt, (a,grad)->nlopt_objective_fn(a, grad,hh))
 ## Define the lower bounds for the parameters:
@@ -215,119 +149,131 @@ opt.xtol_rel     = 1e-10
 ## Perform optimization on the object defined and the initial guess:
 max_f, a_optim, ret = NLopt.optimize(opt, [0.1,0.1,0.1,0.1])
 
-using NLopt
+####### TASK 3: Define and plot the path of optimal consumption #######
+c_1     = 0 #Replace the 0!
+c_2     = 0 #Replace the 0!
+c_3     = 0 #Replace the 0!
+c_4     = 0 #Replace the 0!
+c_5     = 0 #Replace the 0!
+plot([c_1,c_2,c_3,c_4,c_5],xlabel="Period",ylabel="Consumption",label="",lw=3,yaxis=[0,3])
 
-# Define the Household struct and constructor
-struct Household
-    R::Float64       # Rate of return
-    β::Float64       # Discount factor
-    a_0::Float64     # Initial assets
-    y::Vector{Float64}  # Income path
-end
 
-# Define the household object
-hh = Household(1.03, 0.96, 1.0, [5.0, 4.0, 3.0, 2.0, 1.0])
+####### TASK 3: Define and plot the path of optimal consumption #######
+using Plots
 
-# Objective function
-function nlopt_objective_fn(a, grad, hh)
+# Define the function to compute optimal consumption path
+function optimal_consumption_path(a_optim, hh)
     R = hh.R
     a_0 = hh.a_0
     y = hh.y
-    β = hh.β
     
-    # Calculate consumption
-    T = length(y)
-    c = Vector{Float64}(undef, T)
-    c[1] = R * a_0 + y[1] - a[1]
+    T = length(y)  # Number of periods (length of income path)
+    c = Vector{Float64}(undef, T)  # Consumption path for all periods
+    
+    # First period consumption (initial assets + income - asset allocation)
+    c[1] = R * a_0 + y[1] - a_optim[1]  # First period uses initial assets
+    
+    # Subsequent periods' consumption based on asset decisions
     for t in 2:T
-        c[t] = R * a[t-1] + y[t] - a[t]
+        c[t] = R * a_optim[t-1] + y[t] - a_optim[t]  # Use a_optim[t-1] for previous assets
     end
-
-    # Calculate lifetime utility
-    Lifetime_util = 0.0
-    for t in 1:T
-        if c[t] <= 0
-            println("Warning: Non-positive consumption in period $t. Returning -Inf.")
-            return Inf
-        end
-        Lifetime_util += (β^(t-1)) * log(c[t])
-    end
-
-    return -Lifetime_util  # Return negative utility for minimization
+    
+    return c
 end
 
-# Define the dimensionality based on hh.y
-T = length(hh.y)
-dim = T - 1  # Number of decision variables
+# Ensure the dimensionality of the optimization is correct (T-1)
+T = length(hh.y)  # Number of periods
+dim = T - 1  # Dimensionality for asset decisions
 
-# Define the optimizer
-opt = NLopt.Opt(:LN_COBYLA, dim)  # Adjust the dimensionality dynamically
+# Get the optimal asset allocations (a_optim)
+# Assuming that the optimization step has already been performed
+# a_optim will have (T-1) elements, representing assets for each period
 
-# Assign the objective function
-NLopt.max_objective!(opt, (a, grad) -> nlopt_objective_fn(a, grad, hh))
+# Now calculate the optimal consumption path
+c_optimal = optimal_consumption_path(a_optim, hh)
 
-# Set bounds
-opt.lower_bounds = fill(-100.0, dim)  # Lower bounds for each variable
-opt.upper_bounds = fill(15.0, dim)    # Upper bounds for each variable
+# Plot the optimal consumption path
+plot(c_optimal, xlabel="Period", ylabel="Consumption", label="Optimal Consumption", lw=3, ylims=(0, 3), title="Optimal Consumption Path")
 
-# Set stopping criteria
-opt.maxeval = 2000
-opt.xtol_rel = 1e-10
-
-# Initial guess
-initial_guess = fill(0.1, dim)
-
-# Perform optimization
-max_f, a_optim, ret = NLopt.optimize(opt, initial_guess)
-
-# Display results
-println("Maximum Utility: ", max_f)
-println("Optimal Asset Allocations: ", a_optim)
-println("Return Code: ", ret)
-
-
-
-
-####### 
 
 ####################The end of concept check####################
 
-##
+## A generic function which can take:
+## (1) any household object  (hh)
+## (2) any initial guess for the optimizer (initial_a)
+## (3) any lower bound for the optimizer (lower_bound)
+## and solve the consumption-saving problem!
+function solvehh(hh;initial_a_guess=[0.1,0.1,0.1,0.1],lower_bound=-100)
+    function nlopt_objective_fn(a::Vector, grad::Vector,hh)
+        c_1     = hh.R*hh.a_0   + hh.y[1]   -a[1]
+        c_2     = hh.R*a[1]     + hh.y[2]   -a[2]
+        c_3     = hh.R*a[2]     + hh.y[3]   -a[3]
+        c_4     = hh.R*a[3]     + hh.y[4]   -a[4]
+        c_5     = hh.R*a[4]     + hh.y[5]
 
-function plot_paths(C,A,;crange=[0,2],arange=[-2,2])
-    A_path  = plot([A.a1,A.a2,A.a3,A.a4,A.a5],xlabel="Period",ylabel="Level of assets saved",lw=3,yaxis=arange)
+        Lifetime_utility = hh.u(c_1) + hh.β*hh.u(c_2) + hh.β^2*hh.u(c_3) + hh.β^3*hh.u(c_4) + hh.β^4*hh.u(c_5) 
+ 
+        println("Params, Function ",round.(a,digits=5),", ",round(Lifetime_utility,digits=5)) 
+        return Lifetime_utility 
+    end
+    opt = NLopt.Opt(:LN_COBYLA, 4)
+    opt.lower_bounds = [lower_bound,lower_bound,lower_bound,lower_bound]  # lower bound
+    opt.upper_bounds = [15,15,15,15] # lower bound
+    opt.maxeval      = 20000
+    opt.xtol_rel     = 1e-10 # tolerance
+    NLopt.max_objective!(opt, (params,grad)->nlopt_objective_fn(params, grad,hh))
+    max_f, a_optim, ret = NLopt.optimize(opt, initial_a_guess)
+    
+    c1     = hh.R*hh.a_0   + hh.y[1]   -a_optim[1]
+    c2     = hh.R*a_optim[1]     + hh.y[2]   -a_optim[2]
+    c3     = hh.R*a_optim[2]     + hh.y[3]   -a_optim[3]
+    c4     = hh.R*a_optim[3]     + hh.y[4]   -a_optim[4]
+    c5     = hh.R*a_optim[4]     + hh.y[5]
+    C       = (c1=c1,c2=c2,c3=c3,c4=c4,c5=c5)
+    A       = (a1=a_optim[1],a2=a_optim[2],a3=a_optim[3],a4=a_optim[4],a5=0)
+    return C,A
+end
+
+function plot_paths(C,A,hh,;crange=[0,2],arange=[-2,2])
+    A_path  = plot([A.a1,A.a2,A.a3,A.a4,A.a5],xlabel="Period",label="Level of assets saved",lw=3,yaxis=arange)
     hline!([0],label="",ls=:dash)
-    C_path  = plot([C.c1,C.c2,C.c3,C.c4,C.c5],xlabel="Period",ylabel="Consumption",lw=3,yaxis=crange)
-    display(plot(A_path,C_path))
+    C_path  = plot([C.c1,C.c2,C.c3,C.c4,C.c5],xlabel="Period",label="Consumption",lw=3,yaxis=crange)
+    Y_path  = scatter(hh.y,xlabel="Period",label="Income",lw=3,ls=:dash)
+    All_paths = plot(A_path,C_path,Y_path)
+    display(All_paths)
 end
 
 ## Motives for saving: (1) Smoothing motive
 ## Note: each time I create a new HH with different y path, but equal β and R!
 
 ## First example: y path is [1,0,0,0,0]
-C,A = solvehh(create_HH(y=[1,0,0,0,0],β=0.9,R=1/0.9);initial_a=[0.2,0.1,0.1,0.1])
-plot_paths(C,A)
-## Second example: y path is [0,0,1,0,0], be careful about initial_a!
-C,A = solvehh(create_HH(y=[0,0,1,0,0],β=0.9,R=1/0.9);initial_a=[-0.1,-0.2,0.1,0.1])
-plot_paths(C,A)
-## Third example: y path is [0,0,0,0,1], be careful about initial_a!
-C,A = solvehh(create_HH(y=[0,0,0,0,1],β=0.9,R=1/0.9);initial_a=[-0.1,-0.2,-0.3,-0.4])
-plot_paths(C,A)
+hh = create_HH(y=[1,0,0,0,0],β=0.9,R=1/0.9)
+C,A = solvehh(hh;initial_a_guess=[0.2,0.1,0.1,0.1])
+plot_paths(C,A,hh)
+## Second example: y path is [0,0,1,0,0], be careful about initial_a_guess!
+hh = create_HH(y=[0,0,1,0,0],β=0.9,R=1/0.9)
+C,A = solvehh(hh;initial_a_guess=[-0.1,-0.2,0.1,0.1])
+plot_paths(C,A,hh)
+## Third example: y path is [0,0,0,0,1], be careful about initial_a_guess!
+hh = create_HH(y=[0,0,0,0,1],β=0.9,R=1/0.9)
+C,A = solvehh(hh;initial_a_guess=[-0.1,-0.2,-0.3,-0.4])
+plot_paths(C,A,hh)
 
 
 ## Motives for savings: (2) Intertemporal motive:
 ## Note: each time I create a new HH with different β and R, but equal y path!
-
 ##First example: β=0.99,R=1.05; βR>1
-C,A = solvehh(create_HH(y=[1,1,1,1,1],β=0.99,R=1.05);initial_a=[-0.1,-0.2,0.1,0.1])
-plot_paths(C,A)
+hh = create_HH(y=[1,1,1,1,1],β=0.99,R=1.05)
+C,A = solvehh(hh;initial_a_guess=[0.1,0.1,0.1,0.1])
+plot_paths(C,A,hh,arange=[-1,1])
 C.c2/C.c1
 C.c3/C.c2
 C.c4/C.c3
 
 ##Second example: β=0.9,R=1.05; βR<1
-C,A = solvehh(create_HH(y=[1,1,1,1,1],β=0.9,R=1.05);initial_a=[-0.1,-0.2,0.1,0.1])
-plot_paths(C,A)
+hh = create_HH(y=[1,1,1,1,1],β=0.9,R=1.05)
+C,A = solvehh(hh;initial_a_guess=[-0.1,-0.1,-0.1,-0.1])
+plot_paths(C,A,hh,arange=[-1,1])
 
 C.c2/C.c1
 C.c3/C.c2
@@ -335,8 +281,10 @@ C.c4/C.c3
 
 #Note the importance of the borrowing constraint:
 ## No borrowing constraint:
-C,A = solvehh(create_HH(y=[1,3,5,2,1],β=0.9,R=1/0.9);initial_a=[0.2,0.1,0.1,0.1])
-plot_paths(C,A,crange=[0,3],arange=[-2,3])
+hh = create_HH(y=[1,3,5,2,1],β=0.9,R=1/0.9)
+C,A = solvehh(hh;initial_a_guess=[0.1,0.1,0.1,0.1])
+plot_paths(C,A,hh;crange=[0,3],arange=[-2,3])
 ## Borrowing constraint: a>=0
-C,A = solvehh(create_HH(y=[1,3,5,2,1],β=0.9,R=1/0.9);initial_a=[0.2,0.1,0.1,0.1],lower_bound=0.0)
-plot_paths(C,A,crange=[0,3],arange=[-2,3])
+hh = create_HH(y=[1,3,5,2,1],β=0.9,R=1/0.9)
+C,A = solvehh(hh;initial_a_guess=[0.1,0.1,0.1,0.1],lower_bound=0.0)
+plot_paths(C,A,hh;crange=[0,4],arange=[-2,4])
